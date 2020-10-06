5e2b14dcd173ea9d7faced802234dc2898b48225
Pedro Ignacio <pedroig100.pi@gmail.com>
2020-10-06 20:53:48 -0300

    git add *

--- code/app.js
+++ code/app.js
@@ -1,6 +1,25 @@
+/* REFERENCIAS */
+/* header elements */
+const header = document.querySelector("header")
+const headerTitle = document.querySelector("#header-title")
+const round = document.styleSheets[1].rules[12]
+
 /* mudança de cor no slider */
 const slider = document.querySelector(".slider")
 const aux =  document.getElementsByClassName("slider")
+
+/* main elements */
+const main =  document.querySelector("main")
+
+/* primeiros cards */
+const socialCard = document.getElementsByClassName("social-media")
+const numSeguidores = document.querySelectorAll(".numero-seguidores")
+
+/* 8 cards */
+const overviewH1 = document.querySelector("#overview h1")
+const overviewCard = document.getElementsByClassName("overview-card")
+const topicValue = document.getElementsByClassName("topic-value")
+
 
 var ativo = 0
 slider.addEventListener("click", () => {
@@ -17,38 +36,20 @@ slider.addEventListener("click", () => {
     }
 })
 
-/* REFERENCIAS */
-/* header elements */
-const header = document.querySelector("header")
-const headerTitle = document.querySelector("#header-title")
-const round = document.styleSheets[1].rules[12]
-
-/* main elements */
-const main =  document.querySelector("main")
-
-/* primeiros cards */
-const socialCard = document.getElementsByClassName("social-media")
-const numSeguidores = document.querySelectorAll(".numero-seguidores")
-
-/* 8 cards */
-const overviewH1 = document.querySelector("#overview h1")
-const overviewCard = document.getElementsByClassName("overview-card")
-const topicValue = document.getElementsByClassName("topic-value")
-
-/* QUANDO OS SOCIAL CARDS TROCAM DE COR, O :HOVER NÃO FUNCIONA MAIS */
-
 function darkMode(){
     /* ATRIBUIÇÕES */
     header.setAttribute("class", "dark")
     headerTitle.setAttribute("class", "white-text")
     main.setAttribute("class", "dark-main")
     for(let i = 0; i < socialCard.length; i++){
-        socialCard[i].classList.add("dark-card");
+        /* socialCard[i].classList.add("dark-card"); */
+        socialCard[i].style.backgroundColor = "hsl(228, 28%, 20%)";
         numSeguidores[i].style.color = "hsl(0, 0%, 100%)";
     }
     overviewH1.style.color = "hsl(0, 0%, 100%)";
     for(let i = 0; i < overviewCard.length; i++){
-        overviewCard[i].classList.add("dark-card");
+        /* overviewCard[i].classList.add("dark-card"); */
+        overviewCard[i].style.backgroundColor = "hsl(228, 28%, 20%)";
         topicValue[i].style.color = "hsl(0, 0%, 100%)";
     }
     round.style.backgroundColor = "black"
@@ -59,13 +60,32 @@ function removeDarkMode(){
     headerTitle.removeAttribute("class")
     main.removeAttribute("class")
     for(let i = 0; i < socialCard.length; i++){
-        socialCard[i].classList.remove("dark-card");
+        /* socialCard[i].classList.remove("dark-card"); */
+        socialCard[i].style.backgroundColor = "hsl(227, 47%, 96%)";
         numSeguidores[i].style.color = "hsl(230, 17%, 14%)";
     }
     for(let i = 0; i < overviewCard.length; i++){
-        overviewCard[i].classList.remove("dark-card");
+        /* overviewCard[i].classList.remove("dark-card"); */
+        overviewCard[i].style.backgroundColor = "hsl(227, 47%, 96%)";
         topicValue[i].style.color = "hsl(230, 17%, 14%)";
     }
     overviewH1.style.color = "hsl(228, 12%, 44%)";
     round.style.backgroundColor = "hsl(225, 100%, 98%)"
+}
+
+function mudaCor(card){
+    if(ativo !== 0){
+        card.style.backgroundColor = "hsl(230, 22%, 27%)";
+    }
+    else{
+        card.style.backgroundColor = "hsl(223, 15%, 91%)";
+    }
+}
+function corNormal(card){
+    if(ativo !== 0){
+        card.style.backgroundColor = "hsl(228, 28%, 20%)";
+    }
+    else{
+        card.style.backgroundColor = "hsl(227, 47%, 96%)";
+    }
 }
